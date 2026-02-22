7db4beeb9c7d903b0e8cb45364ff01df483316c1
Alexey Milovidov <milovidov@clickhouse.com>
2026-02-22 23:56:56 +0100

    Update 03984_in_single_column_reference.sql

--- tests/queries/0_stateless/03984_in_single_column_reference.sql
+++ tests/queries/0_stateless/03984_in_single_column_reference.sql
@@ -2,6 +2,8 @@
 -- it should still work as a column reference, not fail with
 -- "Function 'in' is supported only if second argument is constant or table expression"
 
+SET enable_analyzer = 1;
+
 SELECT 3 IN (col3) FROM (SELECT 3 AS col3);
 SELECT -99 IN (col3) FROM (SELECT 3 AS col3);
 SELECT col0 IN (col3) FROM (SELECT 1 AS col0, 3 AS col3);
