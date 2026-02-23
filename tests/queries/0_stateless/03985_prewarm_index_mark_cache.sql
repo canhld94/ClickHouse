b94cf0db5f4ff9ee8051bb50db5942c2f4ababde
Anton Popov <anton@clickhouse.com>
2026-02-23 16:11:50 +0000

    allow to prewarm marks of secondary indexes

---
+++ tests/queries/0_stateless/03985_prewarm_index_mark_cache.sql
@@ -0,0 +1,65 @@
+-- Tags: no-parallel, no-shared-merge-tree
+
+DROP TABLE IF EXISTS t_prewarm_idx_cache_1;
+DROP TABLE IF EXISTS t_prewarm_idx_cache_2;
+
+CREATE TABLE t_prewarm_idx_cache_1 (a UInt64, b UInt64, c String, INDEX idx_b b TYPE minmax GRANULARITY 1)
+ENGINE = ReplicatedMergeTree('/clickhouse/tables/{database}/03985_prewarm_index_mark_cache/t', '1')
+ORDER BY a SETTINGS prewarm_mark_cache = 1;
+
+CREATE TABLE t_prewarm_idx_cache_2 (a UInt64, b UInt64, c String, INDEX idx_b b TYPE minmax GRANULARITY 1)
+ENGINE = ReplicatedMergeTree('/clickhouse/tables/{database}/03985_prewarm_index_mark_cache/t', '2')
+ORDER BY a SETTINGS prewarm_mark_cache = 1;
+
+SYSTEM DROP MARK CACHE;
+
+SYSTEM STOP FETCHES t_prewarm_idx_cache_2;
+
+-- Check that prewarm works on insert.
+INSERT INTO t_prewarm_idx_cache_1 SELECT number, number, randomString(10) FROM numbers(20000);
+SELECT count() FROM t_prewarm_idx_cache_1 WHERE b < 10000 AND NOT ignore(*);
+
+-- Check that prewarm works on fetch.
+SYSTEM DROP MARK CACHE;
+SYSTEM START FETCHES t_prewarm_idx_cache_2;
+SYSTEM SYNC REPLICA t_prewarm_idx_cache_2;
+SELECT count() FROM t_prewarm_idx_cache_2 WHERE b < 10000 AND NOT ignore(*);
+
+-- Check that prewarm works on merge.
+INSERT INTO t_prewarm_idx_cache_1 SELECT number, number, randomString(10) FROM numbers(20000);
+OPTIMIZE TABLE t_prewarm_idx_cache_1 FINAL;
+
+SYSTEM SYNC REPLICA t_prewarm_idx_cache_2;
+
+SELECT count() FROM t_prewarm_idx_cache_1 WHERE b < 10000 AND NOT ignore(*);
+SELECT count() FROM t_prewarm_idx_cache_2 WHERE b < 10000 AND NOT ignore(*);
+
+-- Check that prewarm works on restart.
+SYSTEM DROP MARK CACHE;
+
+DETACH TABLE t_prewarm_idx_cache_1;
+DETACH TABLE t_prewarm_idx_cache_2;
+
+ATTACH TABLE t_prewarm_idx_cache_1;
+ATTACH TABLE t_prewarm_idx_cache_2;
+
+SELECT count() FROM t_prewarm_idx_cache_1 WHERE b < 10000 AND NOT ignore(*);
+SELECT count() FROM t_prewarm_idx_cache_2 WHERE b < 10000 AND NOT ignore(*);
+
+SYSTEM DROP MARK CACHE;
+
+SELECT count() FROM t_prewarm_idx_cache_1 WHERE b < 10000 AND NOT ignore(*);
+
+--- Check that system query works.
+SYSTEM PREWARM MARK CACHE t_prewarm_idx_cache_1;
+
+SELECT count() FROM t_prewarm_idx_cache_1 WHERE b < 10000 AND NOT ignore(*);
+
+SYSTEM FLUSH LOGS query_log;
+
+SELECT ProfileEvents['LoadedMarksCount'] > 0 FROM system.query_log
+WHERE current_database = currentDatabase() AND type = 'QueryFinish' AND query LIKE 'SELECT count() FROM t_prewarm_idx_cache%'
+ORDER BY event_time_microseconds;
+
+DROP TABLE IF EXISTS t_prewarm_idx_cache_1;
+DROP TABLE IF EXISTS t_prewarm_idx_cache_2;
