1172efcf17ec0dc14a0de4cbaa6824a6cfa3013d
avogar <avogar@clickhouse.com>
2026-02-24 15:38:28 +0000

    Better test

--- tests/queries/0_stateless/03928_json_advanced_shared_data_bug.sql
+++ tests/queries/0_stateless/03928_json_advanced_shared_data_bug.sql
@@ -1,30 +1,16 @@
--- Tags: long
-
-set max_block_size = 100, min_insert_block_size_rows = 0, min_insert_block_size_bytes = 0, max_insert_threads = 8, max_threads = 8;
-
 DROP TABLE IF EXISTS test;
-CREATE TABLE test (`json` JSON(max_dynamic_paths = 8)) ENGINE = MergeTree ORDER BY tuple() SETTINGS min_bytes_for_wide_part = 1, min_rows_for_wide_part = 1, write_marks_for_substreams_in_compact_parts = 1, object_serialization_version = 'v3', object_shared_data_serialization_version = 'advanced', object_shared_data_serialization_version_for_zero_level_parts = 'advanced', object_shared_data_buckets_for_wide_part = 2, index_granularity=1000;
 
-INSERT INTO
-    test
-SELECT
-    multiIf(
-        number < 15000,
-        '{"?1" : 1, "?2" : 1, "?3" : 1, "?4" : 1, "?5" : 1, "?6" : 1, "?7" : 1, "?8" : 1}',
-        number < 20000,
-        '{"a" : {"a1" : 1, "a2" : 2, "arr" : [{"arr1" : 3, "arr2" : 4, "arr3" : 5, "arr4" : 6}]}, "b" : 7, "c" : 8, "arr" : [{"arr1" : 9, "arr2" : 10, "arr3" : 11, "arr4" : 12}]}',
-        number < 25000,
-        '{}',
-        number < 30000,
-        '{"a" : {"a1" : 3, "a2" : 4, "arr" : [{"arr1" : 5, "arr2" : 6, "arr3" : 7, "arr4" : 8}]}}',
-        number < 35000,
-        '{"b" : 9, "c" : 10}',
-        number < 40000,
-        '{"arr" : [{"arr1" : 11, "arr2" : 12, "arr3" : 13, "arr4" : 14}]}', '{"a" : {"a1" : 5, "a2" : 6}}')
-FROM numbers(45000);
+CREATE TABLE test
+(
+    `json` JSON(max_dynamic_paths = 1)
+)
+ENGINE = MergeTree
+ORDER BY tuple()
+SETTINGS min_bytes_for_wide_part = 1, min_rows_for_wide_part = 1, write_marks_for_substreams_in_compact_parts = 1, object_serialization_version = 'v3', object_shared_data_serialization_version = 'advanced', object_shared_data_serialization_version_for_zero_level_parts = 'advanced', object_shared_data_buckets_for_wide_part = 1, index_granularity = 100;
 
+INSERT INTO test SELECT multiIf(number < 10, '{"a" : [{"b" : 42}]}', number < 20, '{}', '{"a" : [{"b" : 42}]}') from numbers(30);
 
-SELECT json FROM test FORMAT `Null`;
+SELECT * FROM test SETTINGS max_block_size=10;
 
-DROP TABLE IF EXISTS test;
+DROP TABLE test;
 
