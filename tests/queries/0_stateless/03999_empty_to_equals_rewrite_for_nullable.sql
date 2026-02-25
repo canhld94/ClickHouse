bf94fb19da94bada8a5ed32938d311f1d70e6b53
Nikita Taranov <nikita.taranov@clickhouse.com>
2026-02-25 12:37:46 +0000

    impl

---
+++ tests/queries/0_stateless/03999_empty_to_equals_rewrite_for_nullable.sql
@@ -0,0 +1,7 @@
+set allow_experimental_nullable_tuple_type=1;
+
+CREATE TABLE test (`a` Nullable(Tuple(x Array(UInt16))));
+INSERT INTO test SELECT tuple(range(number % 1000)) FROM numbers_mt(1000);
+
+SELECT DISTINCT count() IGNORE NULLS FROM test PREWHERE notEmpty(a.x) WHERE notEmpty(a.x);
+
